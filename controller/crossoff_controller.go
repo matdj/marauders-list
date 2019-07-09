f897ab2ecccbe06c0892f335b95c1e2e8f316638
Mat J <mj506235+gh@gmail.com>
2019-07-10 07:33:39 +1000

    Added service

--- controller/crossoff_controller.go
+++ controller/crossoff_controller.go
@@ -2,13 +2,26 @@ package controller
 
 import (
 	"marauders-list/htmlutil"
+	"marauders-list/services"
 	"marauders-list/domain"
 	"net/http"
 )
 
-var exampleShoppingList = domain.ShoppingList{Items: []domain.Item{domain.Item{Name: "bananas"}}, 
-	CrossedoffItems: []domain.Item{domain.Item{Name: "strawberries"}}}
+var service = new(services.ShoppingListService)
 
 func CrossoffHandler(w http.ResponseWriter, r *http.Request) {
+	addWeetbix()
+
+	var exampleShoppingList = domain.ShoppingList{
+		Items: service.Items(), 
+		CrossedoffItems: service.CrossedoffItems()}
+
 	htmlutil.RenderTemplate(w, "crossoff", exampleShoppingList, htmlutil.ReloadTemplateFn())
+}
+
+func addWeetbix() {
+	weetbix := domain.Item{Name: "weetbix"}
+	if !service.ItemsContains(weetbix) {
+		service.Add(weetbix)
+	}	
 }
